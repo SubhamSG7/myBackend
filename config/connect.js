967224f84ad0d997e259ccbf1d31f592e63693f3
subham-dev07 <subham.das@polariscampus.com>
2026-04-29 17:22:34 +0530

    changed mongoURI

--- config/connect.js
+++ config/connect.js
@@ -1,11 +1,9 @@
- const mongoURL="mongodb+srv://subhamdas_db_user:qwerty123@cluster0.m7khlcr.mongodb.net/?appName=Cluster0"
+require('dotenv').config()
  const mongoose=require('mongoose');
-
-
 
  async function connect() {
     try {
-        await mongoose.connect(mongoURL);
+        await mongoose.connect(process.env.mongoURI);
         console.log('DB connected');
     } catch (error) {
         console.log(error);
